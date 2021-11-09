864a04c1652d6c8970c3ecea48fcbcf0830b23a1
andmansim <91722324+andmansim@users.noreply.github.com>
2021-11-09 16:45:24 +0100

    comit

--- juegoAvanzado.py
+++ juegoAvanzado.py
@@ -28,50 +28,53 @@ if user == 1:
     import random
     number = random.randint(0,1000)
     print(number)
-    print("Choose an integrer between 0 and 99")
+    print("Choose an integrer between 0 and 1000")
     intento_1 = int(input())
     while intento_1 != number:
-        if intento_1 > 99:
-            print("Choose another number between 0 an 99")
+        if intento_1 > 999:
+            print("Choose another number between 0 an 999")
         else:
             if intento_1 > number:
                 print("Too far from the number, it's smaller. The number is between:" + "0 y" + str(intento_1))
             else:
-                print("Too far from the number, it's bigger. The number is between:" + str(intento_1) + "y 99")
+                print("Too far from the number, it's bigger. The number is between:" + str(intento_1) + "y 999")
         intento_1 = int(input())
     if intento_1 == number:
-        print("¡Congratulations!, you have completed level 0")
+        print("¡Congratulations!, you have completed level 1")
 
-    
-   # if user == 0:
-    #    import random
-    #numero = random.randint(0,100)
-    #print(numero)
-    #print("Choose an integer between 0 and 99")
-    #intento_1 = int(input())
-    
-    
+if user == 2:
+    import random
+    number = random.randint(0,1000000)
+    print(number)
+    print("Choose an integrer between 0 and 1000000")
+    intento_1 = int(input())
+    while intento_1 != number:
+        if intento_1 > 999999:
+            print("Choose another number between 0 an 999999")
+        else:
+            if intento_1 > number:
+                print("Too far from the number, it's smaller. The number is between:" + "0 y " + str(intento_1))
+            else:
+                print("Too far from the number, it's bigger. The number is between:" + str(intento_1) + "y 999999")
+        intento_1 = int(input())
+    if intento_1 == number:
+        print("¡Congratulations!, you have completed level 2")
 
-    
-        
-    
-    
-#nivel_medio = random.randint(0,1000)
-#nivel_avanzado = random.randint(0, 1000000)
-#nivel_experto = random.randint(0,1000000000000)
+if user == 3:
+    import random
+    number = random.randint(0,1000000000000)
+    print(number)
+    print("Choose an integrer between 0 and 1000000000000")
+    intento_1 = int(input())
+    while intento_1 != number:
+        if intento_1 > 999999999999:
+            print("Choose another number between 0 an 999999999999")
+        else:
+            if intento_1 > number:
+                print("Too far from the number, it's smaller. The number is between:" + "0 y" + str(intento_1))
+            else:
+                print("Too far from the number, it's bigger. The number is between:" + str(intento_1) + "y 999999999999")
+        intento_1 = int(input())
+    if intento_1 == number:
+        print("¡Congratulations!, you have completed level 3")
 
-#print(numero)
-# print("Intento is an integer")
-# intento_1 = int(input())
-# while intento_1 != numero:
-    
-    #if intento_1 > 99:
-        #print("Choose another number between 0 an 99")
-    #else:
-     #   if intento_1 > numero:
-      #      print("Too far from the number, it's smaller. The number is between:" + "0 y" + str(intento_1))
-       # else:
-        #    print("Too far from the number, it's bigger. The number is between:" + str(intento_1) + "y 99")
-    #intento_1 = int(input())
-#if intento_1 == numero:
- #    print("¡Congratulations!")
